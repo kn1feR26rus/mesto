3a02e59498e27c1ed34475ad10585e0a363c8e73
Oleg Kovalenko <kn1feR26rus@yandex.ru>
2020-08-28 23:16:23 +0300

    Wow1

---
+++ js/Card.js
@@ -0,0 +1,53 @@
+export class Card {
+    constructor(data, templateSelector, imgModal, toggleModalVisible) {
+        this.data = data;
+        this.templateSelector = templateSelector;
+        this.imgModal = imgModal;
+        this.toggleModalVisible = toggleModalVisible;
+    }
+
+    _handleLike(evt){
+        evt.target.classList.toggle('element__like_black');
+    }
+
+    _handleImg(evt) {
+        this.openedImg.setAttribute('src', this.data.link);
+        this.openedImg.setAttribute('alt', this.data.title);
+        this.imgTitle.textContent = this.data.title;
+        this.toggleModalVisible(this.imgModal);
+    }
+
+    _handleRemove(evt) {
+        evt.target.closest('.element').remove();
+    };
+
+    _setEventListeners() {
+        this.elDelete.addEventListener('click', this._handleRemove);
+        this.elLike.addEventListener('click', this._handleLike);
+        this.elImg.addEventListener('click', this._handleImg);
+    }
+
+    createCard() {
+        const Template = document.querySelector(this.templateSelector);
+        this.elCard = Template.content.cloneNode(true);
+        this.elDelete = this.elCard.querySelector('.element__delete');
+        this.elLike = this.elCard.querySelector('.element__like');
+        this.elImg = this.elCard.querySelector('.element__image');
+        this.elTitle = this.elCard.querySelector('.element__text');
+       
+
+        this.elTitle.innerText = this.data.title;
+        this.elImg.src = this.data.link;
+        this.elImg.alt = this.data.title;
+        
+
+        //МОДАЛКА ИЗОБРАЖЕНИЯ
+        this.imgTitle = this.imgModal.querySelector('.imgmodal__title');
+        this.openedImg = this.imgModal.querySelector('.imgmodal__img');
+
+        this._setEventListeners();
+        
+        return elCard;
+        
+    }
+}
